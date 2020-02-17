a813c2b91e7b4fca2ef359923ee0abb40748501b
danielbaptista3 <baptista.daniel3@gmail.com>
2020-02-17 15:54:23 +0100

    add initiative and damage calculation

--- pokemon.js
+++
@@ -1,8 +0,0 @@
-var Pokemon = /** @class */ (function () {
-    function Pokemon(name) {
-        this.name = name;
-    }
-    return Pokemon;
-}());
-var pikachu = new Pokemon("Pikachu");
-console.log(pikachu.name);
