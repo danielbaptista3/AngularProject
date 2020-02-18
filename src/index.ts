54a2f1fe5a40b841df757435c83760ef28291499
danielbaptista3 <baptista.daniel3@gmail.com>
2020-02-18 14:09:12 +0100

    cc

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,11 @@ let charmander = new Pokemon("Charmander", 5, 35, 8, 14, 12, 8, 7, charmanderMov
 let squirtleMoves = [new Move("Tackle", 40, Category.Physic, 1), new Move("Water Gun", 40, Category.Special, 1)];
 let squirtle = new Pokemon("Squirtle", 5, 38, 7, 12, 11, 10, 9, squirtleMoves);
 
-while(true)
+let isFinished = false;
+
+while(isFinished)
 {
+    
     let battle = new Battle(charmander, squirtle);
     console.log("The battle between " + charmander.name + " and " + squirtle.name + " starts now !");
     
@@ -18,9 +21,19 @@ while(true)
     let randomNumber = Math.floor(Math.random() * 2);
     battle.attack(firstPokemon, firstPokemon.move[randomNumber]);
 
-    //checkVictory();
     randomNumber = Math.floor(Math.random() * 2);
-    battle.attack(//otherPokémon, otherPokémon.move[randomNumber]);
-    //checkVictory();
+    battle.attack(undefined,null);//otherPokémon, otherPokémon.move[randomNumber]);
+
+    if (charmander.hp <=0)
+    {
+        console.log(charmander.name + " has won !");
+        isFinished = true;
+    }
+
+    if (squirtle.hp <=0)
+    {
+        console.log(squirtle.name + " has won !");
+        isFinished = true;
+    }
 
 }
